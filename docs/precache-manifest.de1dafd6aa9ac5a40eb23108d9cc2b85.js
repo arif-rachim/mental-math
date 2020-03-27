8b2d8808e6b1c25a1613fdd43b6b98922cb8cb15
arif <marsa.emreef@gmail.com>
2020-03-27 17:59:36 +0400

    update to support ios

---
+++ docs/precache-manifest.de1dafd6aa9ac5a40eb23108d9cc2b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908b012b726defcfd981abe0dab82587",
+    "url": "/mental-math/index.html"
+  },
+  {
+    "revision": "340b81a1d633f77cf165",
+    "url": "/mental-math/static/css/main.83a228f5.chunk.css"
+  },
+  {
+    "revision": "53625b4be9df58e94544",
+    "url": "/mental-math/static/js/2.13c32bd7.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/mental-math/static/js/2.13c32bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340b81a1d633f77cf165",
+    "url": "/mental-math/static/js/main.e9feadd0.chunk.js"
+  },
+  {
+    "revision": "ac3a11a4eb0108d79aac",
+    "url": "/mental-math/static/js/runtime-main.be99d159.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8689e4a2fcae6c63d0ed57856c685899.js"
+  "/mental-math/precache-manifest.de1dafd6aa9ac5a40eb23108d9cc2b85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mental-math/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
